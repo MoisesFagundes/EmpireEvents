c67cb8c5057d3c87a95a3c54624c134cad85bcad
MoisesFagundes <40305005+MoisesFagundes@users.noreply.github.com>
2022-06-26 16:21:51 -0300

    Caixa aceitar cookies

--- application/views/Montar.php
+++ application/views/Montar.php
@@ -14,7 +14,16 @@
 	    <header>		                    
 			<?php $this->load->view('Template/Header') ?>  
 		</header>         
-            <?php $this->load->view('Meio/'.$Page) ?> 
+            <?php $this->load->view('Meio/'.$Page) ?>
+
+			<div class="cookie-disclaimer">
+				<div class="cookie-close accept-cookie"><i class="fa fa-times"></i></div>
+				<div class="container">
+					<p>A Empire Events usa cookies para aumentar a qualidade do site. <a href="<?= base_url('Politica_de_Privacidade') ?>" target="_blank" >Leia mais sobre nossa política de privacidade.</a>
+					<br>Ao continuar a usar o site, você aceita nosso uso de cookies.</p>
+					<button type="button" class="btn btn-warning accept-cookie">Está bem!</button>
+				</div>
+			</div> 
 		<footer>
 		    <?php $this->load->view('Template/Footer') ?>
 		</footer>		        
@@ -23,7 +32,49 @@
 				$('#preloader .inner').fadeOut();
 				$('#preloader').delay(10).fadeOut('slow'); 
 				$('body').delay(10).css({'overflow': 'visible'});
-			})	
+			})
+			
+			$(document).ready(function() { 
+				var cookie = false;
+				var cookieContent = $('.cookie-disclaimer');
+
+				checkCookie();
+
+				if (cookie === true) {
+				cookieContent.hide();
+				}
+
+				function setCookie(cname, cvalue, exdays) {
+				var d = new Date();
+				d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+				var expires = "expires=" + d.toGMTString();
+				document.cookie = cname + "=" + cvalue + "; " + expires;
+				}
+
+				function getCookie(cname) {
+				var name = cname + "=";
+				var ca = document.cookie.split(';');
+				for (var i = 0; i < ca.length; i++) {
+					var c = ca[i].trim();
+					if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+				}
+				return "";
+				}
+
+				function checkCookie() {
+				var check = getCookie("acookie");
+				if (check !== "") {
+					return cookie = true;
+				} else {
+					return cookie = false; //setCookie("acookie", "accepted", 365);
+				}
+				
+				}
+				$('.accept-cookie').click(function () {
+				setCookie("acookie", "accepted", 365);
+				cookieContent.fadeOut('slow');
+				});
+			});
 		</script>
 
         <?php $this->load->view('Template/Js') ?>
